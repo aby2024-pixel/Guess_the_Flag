ea94e64c9f80e7932e4ee7053273dd41da9af8a6
aby2024-pixel <a01@jaguar.tamu.edu>
2025-06-09 19:38:43 -0500

    Initial Commit

---
+++ Guess_the_Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess_the_Flag
+//
+//  Created by aby on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
