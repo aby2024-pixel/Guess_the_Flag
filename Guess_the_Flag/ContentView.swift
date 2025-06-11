b7708b45a4ad278d73aa9f7f3428ca5cb44f0315
aby2024-pixel <a01@jaguar.tamu.edu>
2025-06-11 16:14:37 -0500

    project update - No.1

--- Guess_the_Flag/ContentView.swift
+++ Guess_the_Flag/ContentView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Button 1"){ }
+                .buttonStyle(.bordered)
+            Button("Button 2", role : .destructive){}
+                .buttonStyle(.bordered)
+            Button("Button 3"){ }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            Button("button 4", role: .destructive){}
+                .buttonStyle(.borderedProminent)
+            Button{
+                print("Edit Button was tapped")
+            }label : {
+                Label("Edit",systemImage: "pencil")
+                    .padding()
+                    .foregroundColor(.blue)
+                    .background(Color.yellow)
+            }
+            
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
